d75275e16bd54ebc7c4516522f2357a46235444a
西川継延 <nishikawakeien@nishikawatsugunobenoMacBook-Pro.local>
2017-12-30 11:12:08 +0900

    Initial Commit

---
+++ alert_picker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  alert_picker
+//
+//  Created by 西川継延 on 2017/12/30.
+//  Copyright © 2017年 西川継延. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
